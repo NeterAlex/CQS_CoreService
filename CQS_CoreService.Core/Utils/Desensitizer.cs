d62a1a78721cb4ace506fef7ae9ecf849284753e
NeterAlex <neteralex@outlook.com>
2023-09-26 21:28:39 +0800

    feat: 新增脱敏工具类

---
+++ CQS_CoreService.Core/Utils/Desensitizer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CQS_CoreService.Core.Utils;
+
+public class Desensitizer
+{
+    public static string GetSafePhone(string phone)
+    {
+        if (phone is null or "")
+            return null;
+        return string.Concat("******", phone.AsSpan(phone.Length - 4));
+    }
+
+    public static string GetSafeIdNumber(string idNumber)
+    {
+        if (idNumber is null or "")
+            return null;
+        return string.Concat(
+            idNumber.AsSpan(0, 3),
+            new string('*', idNumber.Length - 5),
+            idNumber.AsSpan(idNumber.Length - 2));
+    }
+}

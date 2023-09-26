041c64205f8d5f9fa7675a157450c044ad5706a6
NeterAlex <neteralex@outlook.com>
2023-09-26 21:29:32 +0800

    feat: 新增SoilData测试

---
+++ CQS_CoreService.Test/Tests/SoilDataServiceTest.cs
@@ -0,0 +1,26 @@
+﻿using CQS_CoreService.Application;
+using StackExchange.Profiling.Internal;
+using Xunit.Abstractions;
+
+namespace CQS_CoreService.Test;
+
+public class SoilDataServiceTest
+{
+    private readonly ISoilDataService _soilDataService;
+    private readonly ITestOutputHelper _testOutputHelper;
+
+    public SoilDataServiceTest(ISoilDataService soilDataService, ITestOutputHelper testOutputHelper)
+    {
+        _soilDataService = soilDataService;
+        _testOutputHelper = testOutputHelper;
+    }
+
+    [Theory]
+    [InlineData(2)]
+    public async Task GetSoilDataFromUserGroup(int userGroupId)
+    {
+        var result = await _soilDataService.GetSoilDataFromUserGroup(userGroupId);
+        _testOutputHelper.WriteLine(result.ToJson());
+        Assert.NotNull(result);
+    }
+}

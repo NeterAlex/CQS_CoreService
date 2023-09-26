b310d37391baf83611a739dec07028eca9fde74b
NeterAlex <neteralex@outlook.com>
2023-09-26 21:29:22 +0800

    feat: 新增service

--- CQS_CoreService.Application/Services/SoilDataService.cs
+++ CQS_CoreService.Application/Services/SoilDataService.cs
@@ -12,6 +12,10 @@ public interface ISoilDataService
 {
     public Task<SoilDataEntity> CreateSoilData(int userId, string location, string description, IFormFile rawFile,
         IFormFile regionFile);
+
+    public Task<List<SoilDataEntity>> GetSoilDataFromUserGroup(int userGroupId, int pageSize = 50, int pageIndex = 1);
+    public Task<SoilDataEntity> GetSoilData(int soilDataId);
+    public Task<List<SoilDataEntity>> GetSoilDataListFromUser(int userId, int pageSize = 50, int pageIndex = 1);
 }
 
 public class SoilDataService : ISoilDataService, ITransient
@@ -23,6 +27,16 @@ public class SoilDataService : ISoilDataService, ITransient
         _db = db;
     }
 
+    /// <summary>
+    ///     根据数据文件创建计算耕地数据
+    /// </summary>
+    /// <param name="userId"></param>
+    /// <param name="location"></param>
+    /// <param name="description"></param>
+    /// <param name="rawFile"></param>
+    /// <param name="regionFile"></param>
+    /// <returns></returns>
+    /// <exception cref="FileLoadException"></exception>
     public async Task<SoilDataEntity> CreateSoilData(int userId, string location, string description, IFormFile rawFile,
         IFormFile regionFile)
     {
@@ -71,6 +85,82 @@ public class SoilDataService : ISoilDataService, ITransient
         }
     }
 
+    /// <summary>
+    ///     分页获取指定用户的所有耕地数据
+    /// </summary>
+    /// <param name="userId"></param>
+    /// <param name="pageSize"></param>
+    /// <param name="pageIndex"></param>
+    /// <returns></returns>
+    public async Task<List<SoilDataEntity>> GetSoilDataListFromUser(int userId, int pageSize = 50, int pageIndex = 1)
+    {
+        try
+        {
+            RefAsync<int> count = 0;
+            var list = await _db.Queryable<SoilDataEntity>()
+                .Includes(i => i.Owner)
+                .Where(i => i.OwnerId == userId)
+                .ToPageListAsync(pageIndex, pageSize, count);
+            return list;
+        }
+        catch (Exception e)
+        {
+            e.Message.LogError();
+            throw;
+        }
+    }
+
+    /// <summary>
+    ///     获取指定UserGroup下的所有耕地数据
+    /// </summary>
+    /// <param name="userGroupId"></param>
+    /// <param name="pageSize"></param>
+    /// <param name="pageIndex"></param>
+    /// <returns></returns>
+    public async Task<List<SoilDataEntity>> GetSoilDataFromUserGroup(int userGroupId, int pageSize = 50, int pageIndex = 1)
+    {
+        try
+        {
+            RefAsync<int> count = 0;
+            var list = await
+                _db.Queryable<SoilDataEntity, UserEntity>((d, u) => d.OwnerId == u.Id)
+                    .LeftJoin<UserGroupEntity>((d, u, ug) => u.UserGroups.Contains(ug))
+                    .Where((d, u, ug) => ug.Id == userGroupId)
+                    .Select((d, u) => d)
+                    .ToPageListAsync(pageIndex, pageSize, count);
+            return list;
+        }
+        catch (Exception e)
+        {
+            e.Message.LogError();
+            throw;
+        }
+    }
+
+    /// <summary>
+    ///     获取指定的耕地数据
+    /// </summary>
+    /// <param name="soilDataId"></param>
+    /// <returns></returns>
+    public async Task<SoilDataEntity> GetSoilData(int soilDataId)
+    {
+        try
+        {
+            var result = await _db.Queryable<SoilDataEntity>().Where(i => i.Id == soilDataId).SingleAsync();
+            return result;
+        }
+        catch (Exception e)
+        {
+            e.Message.LogError();
+            throw;
+        }
+    }
+
+    /// <summary>
+    ///     请求PyServer
+    /// </summary>
+    /// <param name="rawFile"></param>
+    /// <returns></returns>
     private async Task<PyQueryResultDto> QueryPyServer(byte[] rawFile)
     {
         try

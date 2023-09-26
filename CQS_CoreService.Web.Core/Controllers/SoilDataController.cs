fb8505b5dba4c36ae3a0cb4fc6f0e4fdcc950082
NeterAlex <neteralex@outlook.com>
2023-09-26 21:29:06 +0800

    feat: 新增接口

--- CQS_CoreService.Web.Core/Controllers/SoilDataController.cs
+++ CQS_CoreService.Web.Core/Controllers/SoilDataController.cs
@@ -1,13 +1,18 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using CQS_CoreService.Application;
 using CQS_CoreService.Core.Dto;
+using CQS_CoreService.Core.Entity;
 using Mapster;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CQS_CoreService.Web.Core.Controllers;
 
+[Microsoft.AspNetCore.Components.Route("api/soildata")]
+[ApiDescriptionSettings(Tag = "土地质量数据 SoilData API")]
 [DynamicApiController]
 public class SoilDataController
 {
@@ -18,8 +23,18 @@ public class SoilDataController
         _soilDataService = soilDataService;
     }
 
-    [HttpPost]
-    public async Task<SoilDataSimpleDto> CreateSoilData([FromForm] int userId, [FromForm] string location,
+    /// <summary>
+    ///     根据数据源文件与区域文件计算耕地质量数据
+    /// </summary>
+    /// <param name="userId"></param>
+    /// <param name="location"></param>
+    /// <param name="description"></param>
+    /// <param name="rawFile"></param>
+    /// <param name="regionFile"></param>
+    /// <returns></returns>
+    /// <exception cref="Exception"></exception>
+    [HttpPost("calc")]
+    public async Task<SoilDataSimpleDto> Calc([FromForm] int userId, [FromForm] string location,
         [FromForm] string description,
         IFormFile rawFile,
         IFormFile regionFile)
@@ -28,5 +43,36 @@ public class SoilDataController
         if (resultSoilData is null) throw new Exception("数据计算失败");
         var result = resultSoilData.Adapt<SoilDataSimpleDto>();
         return result;
+    }
+
+    /// <summary>
+    ///     获取指定耕地质量数据
+    /// </summary>
+    /// <param name="dataId"></param>
+    /// <returns></returns>
+    [HttpGet("single")]
+    public async Task<SoilDataEntity> GetSingle(int dataId)
+    {
+        var result = await _soilDataService.GetSoilData(dataId);
+        return result;
+    }
+
+    /// <summary>
+    ///     获取指定用户的所有耕地质量数据
+    /// </summary>
+    /// <param name="userId"></param>
+    /// <returns></returns>
+    [HttpGet("list-user")]
+    public async Task<List<SoilDataSimpleDto>> GetListFromUser(int userId)
+    {
+        var list = await _soilDataService.GetSoilDataListFromUser(userId);
+        return list.Select(i => i.Adapt<SoilDataSimpleDto>()).ToList();
+    }
+
+    [HttpGet("list-group")]
+    public async Task<List<SoilDataSimpleDto>> GetListFromUserGroup(int userGroupId)
+    {
+        var list = await _soilDataService.GetSoilDataFromUserGroup(userGroupId);
+        return list.Select(i => i.Adapt<SoilDataSimpleDto>()).ToList();
     }
 }

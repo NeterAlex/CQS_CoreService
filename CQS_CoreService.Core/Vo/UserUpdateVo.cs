009836d29491fd45d5517cbec34d0e1f7f265ef1
NeterAlex <neteralex@outlook.com>
2023-09-27 20:51:36 +0800

    feat: 添加VO

---
+++ CQS_CoreService.Core/Vo/UserUpdateVo.cs
@@ -0,0 +1,10 @@
+﻿namespace CQS_CoreService.Core.Vo;
+
+public class UserUpdateVo
+{
+    public string Address { get; set; }
+    public string Email { get; set; }
+    public string Fullname { get; set; }
+    public string State { get; set; }
+    public string IdNumber { get; set; }
+}
